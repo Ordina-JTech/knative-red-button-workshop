f418f1c6c9127a615050a727c386e08e0388e65c
Tideman <tijmenbuis@gmail.com>
2019-11-02 15:44:53 +0100

    make it work on Windows.

--- container-execute.sh
+++ container-execute.sh
@@ -3,6 +3,6 @@ set -e
 
 echo "Starting  at: $(date +"%T")"
 echo "Fire docker command"
-docker exec -d rb-button sh /scripts/deploy.sh
+docker exec -d rb-button sh //volume-docker/deploy.sh
 
 exit 0
